8d29bf854cc61336fe92fd709b63f2c00a75dc9f
Micheal Cole Young <coleyoungmusic@gmail.com>
2025-04-20 19:53:09 -0500

    Removed comment for overlay and commented livereload code

--- app.py
+++ app.py
@@ -97,9 +97,10 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.debug = True
-    server = Server(app.wsgi_app)
-    server.watch('templates/**/*.html')
-    server.watch('static/**/*.css')
-    server.watch('app.py')
-    server.serve(port=5000, host='127.0.0.1')
+    # app.debug = True
+    # server = Server(app.wsgi_app)
+    # server.watch('templates/**/*.html')
+    # server.watch('static/**/*.css')
+    # server.watch('app.py')
+    # server.serve(port=5000, host='127.0.0.1')
+    app.run()
